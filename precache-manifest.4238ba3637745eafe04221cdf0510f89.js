62b33507284fd1ff3eaa3d2a7df534c6e4e78f18
mambu1998 <mambufrancis1998@gmail.com>
2022-01-24 10:49:07 -0500

    Updates

--- precache-manifest.4238ba3637745eafe04221cdf0510f89.js
+++ precache-manifest.4238ba3637745eafe04221cdf0510f89.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aeffada7a603bcff33d5cdff4112e8bb",
+    "revision": "52c1a0a33cab248f91bfc76cecc0ebaf",
     "url": "/Benji-Challenge-20final-version/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Benji-Challenge-20final-version/static/css/2.5544c887.chunk.css"
   },
   {
-    "revision": "b87577272b579e22a05d",
+    "revision": "c9677165c6c6917c16d3",
     "url": "/Benji-Challenge-20final-version/static/css/main.4900d245.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Benji-Challenge-20final-version/static/js/2.6fd47ff1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b87577272b579e22a05d",
-    "url": "/Benji-Challenge-20final-version/static/js/main.6749f548.chunk.js"
+    "revision": "c9677165c6c6917c16d3",
+    "url": "/Benji-Challenge-20final-version/static/js/main.ff893611.chunk.js"
   },
   {
     "revision": "a80ba144db46d25b0d38",
